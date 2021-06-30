420281a6f1403312d85d3c61f883f3e3b0fcbef9
taniro <tanirocr@gmail.com>
2021-06-30 13:20:43 -0300

    Initial commit

---
+++ src/main/java/tads/eaj/ufrn/demoaulaimplantacao/DemoAulaImplantacaoApplication.java
@@ -0,0 +1,13 @@
+package tads.eaj.ufrn.demoaulaimplantacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAulaImplantacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAulaImplantacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tads/eaj/ufrn/demoaulaimplantacao/DemoAulaImplantacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package tads.eaj.ufrn.demoaulaimplantacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAulaImplantacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
