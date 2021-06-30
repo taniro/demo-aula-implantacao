0001e3374e23fe2ff0e9cfe7a5a412ec6ebbfafd
taniro <tanirocr@gmail.com>
2021-06-30 13:47:12 -0300

    update database

---
+++ src/main/java/tads/eaj/ufrn/demoaulaimplantacao/controllers/ConectaBanco.java
@@ -0,0 +1,22 @@
+package tads.eaj.ufrn.demoaulaimplantacao.controllers;
+
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConectaBanco {
+
+	static Connection getConnection() throws SQLException, URISyntaxException {
+		String dbUri = System.getenv("DATABASE_HOST");
+		String dbPort = System.getenv("DATABASE_PORT");
+		String dbName = System.getenv("DATABASE_NAME");
+
+		String username = System.getenv("DATABASE_USERNAME");
+		String password = System.getenv("DATABASE_PASSWORD");
+		String dbUrl = "jdbc:postgresql://" + dbUri + ':' + dbPort + "/" + dbName + "?serverTimezone=UTC";
+
+		return DriverManager.getConnection(dbUrl, username, password);
+	}
+}
--- src/main/java/tads/eaj/ufrn/demoaulaimplantacao/controllers/HelloController.java
+++ src/main/java/tads/eaj/ufrn/demoaulaimplantacao/controllers/HelloController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @Controller
 @RequestMapping("/hello")
@@ -15,6 +18,29 @@ public class HelloController {
 
 	@GetMapping
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-		response.getWriter().println("hello gente bonita");
+
+		String name = request.getParameter("nome");
+		response.getWriter().append("Hello, ").append(name).append(".");
+
+		Connection connection = null;
+		try {
+			connection = ConectaBanco.getConnection();
+		} catch (SQLException | URISyntaxException ex) {
+			response.getWriter().append("Connection Failed! Check output console");
+		}
+
+		if (connection != null) {
+			response.getWriter().append("A conexão com o banco foi realizada!");
+		} else {
+			response.getWriter().append("A conexão com o banco falhou!");
+		}
+
+		try {
+			assert connection != null;
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
